baeefbe796c9242916b53e4b8ac858d32c8b1e40
Victoria Fuselier <vic.fuselier@gmail.com>
2023-05-20 15:14:21 -0500

    Added phrases to an array called phrasesToGuess
    Per instructions step #4

--- js/app.js
+++ js/app.js
@@ -2,6 +2,19 @@ const letterButtons = document.querySelector('#qwerty');
 const phraseToGuess = document.querySelector('#phrase');
 let wrongGuesses = 0;
 const startButton = document.querySelector('.btn__reset');
+const phrases = [
+    'FOOL ME ONCE SHAME ON YOU FOOL ME TWICE SHAME ON ME',
+    'HOGWARTS SCHOOL OF WITCHCRAFT AND WIZARDRY',
+    'ANYTHING IS POSSIBLE',
+    'NO PLACE LIKE HOME',
+    'JUST KEEP SWIMMING',
+    'YOU IS KIND YOU IS SMART YOU IS IMPORTANT',
+    'HASTA LA VISTA BABY',
+    'INCONCEIVABLE',
+    'TO INFINITY AND BEYOND',
+    'YOU HAD ME AT HELLO',
+    'ABRACADABRA'
+];
 
 startButton.addEventListener('click', () => {
     const overlay = document.querySelector('#overlay');
